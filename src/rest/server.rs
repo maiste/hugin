979f9cd142b04fd62dc61fbd1be19ee7afdf6f20
Etienne MARAIS <etiennemarais@maiste.fr>
2021-01-04 18:06:01 +0100

    feature: test path
    replace ping with json test feature

--- src/rest/server.rs
+++ src/rest/server.rs
@@ -1,17 +1,21 @@
 /// This Source Code Form is subject to the terms of the Mozilla Public
 /// License, v. 2.0. If a copy of the MPL was not distributed with this
 /// file, You can obtain one at https://mozilla.org/MPL/2.0/.
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{body::Body, get, App, HttpResponse, HttpServer, Responder};
 
-#[get("/ping")]
-pub async fn ping() -> impl Responder {
-    HttpResponse::Ok().body("Pong!")
+#[get("/test")]
+pub async fn test() -> impl Responder {
+    let content = String::from("{ \"test\":\"OK\"}");
+    let body = Body::from(content);
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(body)
 }
 
 #[actix_web::main]
 pub async fn run(port: String) -> std::io::Result<()> {
     let addr = format!("localhost:{}", port);
-    HttpServer::new(|| App::new().service(ping))
+    HttpServer::new(|| App::new().service(test))
         .bind(addr)?
         .run()
         .await
