80a4979e99ae50e6bed3e8601cc934cbbffea307
Etienne MARAIS <etiennemarais@maiste.fr>
2021-01-06 09:08:35 +0100

    feature: get json

--- src/rest/server.rs
+++ src/rest/server.rs
@@ -1,7 +1,7 @@
 /// This Source Code Form is subject to the terms of the Mozilla Public
 /// License, v. 2.0. If a copy of the MPL was not distributed with this
 /// file, You can obtain one at https://mozilla.org/MPL/2.0/.
-use actix_web::{body::Body, get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{body::Body, get, guard, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/test")]
 pub async fn test() -> impl Responder {
@@ -12,11 +12,29 @@ pub async fn test() -> impl Responder {
         .body(body)
 }
 
+#[get("/{name}")]
+async fn json_loader(web::Path(name): web::Path<String>) -> impl Responder {
+    let content = String::from(format!("{{ \"name\" : \"{}\" }}", name));
+    let body = Body::from(content);
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(body)
+}
+
 #[actix_web::main]
 pub async fn run(port: String) -> std::io::Result<()> {
     let addr = format!("localhost:{}", port);
-    HttpServer::new(|| App::new().service(test))
-        .bind(addr)?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .default_service(
+                web::route()
+                    .guard(guard::Not(guard::Get()))
+                    .to(|| HttpResponse::NotFound()),
+            )
+            .service(test)
+            .service(json_loader)
+    })
+    .bind(addr)?
+    .run()
+    .await
 }
