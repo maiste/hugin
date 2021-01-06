8cd06261b9d4fa39f8a3275495f3dfb9182cc954
Etienne MARAIS <etiennemarais@maiste.fr>
2021-01-06 12:05:15 +0100

    feature: config path manager

--- src/rest.rs
+++ src/rest.rs
@@ -2,3 +2,7 @@
 /// License, v. 2.0. If a copy of the MPL was not distributed with this
 /// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 pub mod server;
+
+mod config;
+pub use config::get_config;
+pub use config::set_config;
